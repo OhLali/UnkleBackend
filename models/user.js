37161e38b2d23282e45ecc1c50ef1a282689830e
LaureLine <laureline.rogerieux@gmail.com>
2022-04-17 11:06:06 +0200

    creation userSchema

---
+++ models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var UserSchema = mongoose.Schema({
+    userType : String,
+    email : String,
+    password : String,
+    token : String,
+    contract : { type: mongoose.Schema.Types.ObjectId, ref: 'contacts' },
+})
+
+module.exports = mongoose.model('users', UserSchema)
